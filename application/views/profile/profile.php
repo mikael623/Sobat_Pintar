868284abc8bf386563d6214bfc9dc3762c0e5b82
syarifahais <syarifatulaisyiyah@gmail.com>
2021-04-01 14:17:57 +0700

    profile.html

---
+++ application/views/profile/profile.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sobat Pintar | Daftar Materi</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  <!-- Font Awesome -->
+  <link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+  rel="stylesheet"
+  />
+  <!-- Google Fonts -->
+  <link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+  />
+  <!-- MDB -->
+  <link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css"
+  rel="stylesheet"
+  />
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index.html" class="nav-link">Beranda</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Tentang Admin</a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-comments"></i>
+          <span class="badge badge-danger navbar-badge">3</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="./dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Brad Diesel
+                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">Call me whenever you can...</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  John Pierce
+                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">I got your message bro</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Nora Silvester
+                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">The subject goes here</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Sobat Pintar</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Admin Pertama</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="./index.html" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <!-- <i class="right fas fa-angle-left"></i> -->
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Input Data
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/materi/daftar_materi.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Materi</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/soal/daftar_soal.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Soal</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="container">
+    <div class="card mt-3">
+        <div class="card-header">
+        Profile
+    </div>
+    <div class="bg-light">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-10 mt-5 pt-5">
+                <div class="row z-depth-3">
+                    <div class="col-sm-4 bg-info rounded-left">
+                        <div class="card-block text-center text-white">
+                            <i class="fas fa-user-tie fa-7x mt-5"></i>
+                            <h2 class="font-weight-bold mt-4">Nama</h2>
+                            <p>Web Designer</p>
+                            <i class="far fa-edit fa-2x mt-4"></i>
+                        </div>
+                    </div>
+                    <div class="col-sm-8 bg-white rounded-right">
+                        <h3 class="mt-3 text-center">Information</h3>
+                        <hr class="badge-primary mt-0 w-100">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <p class="font-weight-bold">Gender :</p>
+                                    <h6 class="text-muted">Female</h6>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p class="font-weight-bold">Location :</p>
+                                    <h6 class="text-muted">Bandung</h6>
+                                </div>
+                            </div>
+                            <h4 class="mt-3">Projects</h4>
+                            <hr class="bg-primary">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <p class="font-weight-bold">Recent :</p>
+                                    <h6 class="text-muted">Dashboard Web</h6>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p class="font-weight-bold">Earlier :</p>
+                                    <h6 class="text-muted">Dashboard Web</h6>
+                                </div>
+                            </div>
+                        <hr class="bg-primary">
+                        <ul class="list-unstyled d-flex justify-content-center mt-4">
+                            <li><a href="#"><i class="fab fa-facebook-f px-3 h4 text-info"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin px-3 h4 text-info"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter px-3 h4 text-info"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    <!-- <div class="card-body">
+            <img style="height: 20px; width: 20px" src="/assets/user.png" alt="profile">
+            <h1>Nama</h1>
+            <p>Job</p>
+            <a href="https://www.facebook.com/"><img style="height: 20px; width: 20px" src="/assets/user.png" alt="facebook"></a>
+            <a href="https://www.linkedin.com/"><img style="height: 20px; width: 20px" src="/assets/user.png" alt="linkedin"></a>
+        
+                <p class="field">Gander</p> -->
+                <!-- <p>Gander : Female</p>
+         -->
+                <!-- <p class="field">Age</p> -->
+                <!-- <p> Age : 21</p>
+         -->
+                <!-- <p class="field">Location</p> -->
+                <!-- <p>Location : Bandung</p>
+    </div> --> 
+
+        <div class="card mt-3">
+            <div class="card-body">
+            <form method="post" action="">
+            <div class="form-group">
+                <label>Nama</label>
+                <div class="input-group mb-3">
+                    <input class="form-control" type="text" name="nama" value="">
+                </div>
+                <label>Role</label>
+                <div class="input-group mb-3">
+                    <input class="form-control" type="text" name="role" value="">
+                </div>
+                <label>Gander</label>
+                <div class="form-check">
+                <input type="radio" id="male" name="gender" value="male">
+                <label for="male">Male</label><br>
+                <input type="radio" id="female" name="gender" value="female">
+                <label for="female">Female</label>
+                </div>
+                <br>
+                <label>Location</label>
+                <div class="input-group mb-3">
+                    <input class="form-control" type="text" name="location" value="">
+                </div>
+                <!-- project -->
+                <label>Recent</label>
+                <div class="input-group mb-3">
+                    <input class="form-control" type="text" name="recent" value="">
+                </div>
+                <label>Earlier</label>
+                <div class="input-group mb-3">
+                    <input class="form-control" type="text" name="earlier" value="">
+                </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <a href="#" name="submit" class="btn btn-primary btn-sm" style="border-radius: 8px;">Submit</a>
+                    </div>
+            </div>
+        </form>
+        </div>
+    </div>
+    <!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+</body>
+</html>
+
